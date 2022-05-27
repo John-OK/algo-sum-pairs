75b5f8b4b990119be5c051e06d0b2e5c29fefbd1
JohnOK <john.ok.esp@gmail.com>
2022-05-27 02:19:18 +0200

    added sum_pairs

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,26 @@
-exports.sumPairs = function() {
+var shallowEqualArrays = require('shallow-equal/arrays');
 
+
+// exports.
+sumPairs = function(num_array, target) {
+    let pairs = [];
+    // uniquePairs = [];
+    for (let i = 0; i < num_array.length - 1; i++) {
+        let bind = num_array[i];
+        for (let j = i + 1; j < num_array.length; j++) {
+            let comp = num_array[j]
+            if (bind + comp === target) {
+                pairs.push([bind, comp]);
+            }
+        }
+    }
+
+    if (pairs.length === 0) {
+        return 'unable to find pairs';
+    } else {
+        return pairs;
+    }
 };
+
+// console.log(sumPairs([1,2,3,4,5], 7))
+// console.log(sumPairs(([1,2,3,4,5], 7)))
--- js/sumPairsSpec.js
+++ js/sumPairsSpec.js
@@ -1,3 +1,4 @@
 var sp = require("./sumPairs");
 
 // Don't forget to add your tests :)
+console.log("everything is great!")
