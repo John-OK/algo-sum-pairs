b18b23f546ce358be38417ba43d7ac2c8afd965d
John OKeeffe <john.ok.esp@gmail.com>
2022-05-30 14:32:34 -0500

    added function calls at bottom for testing

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -18,5 +18,6 @@ exports.sumPairs = function(num_array, target) {
     }
 };
 
-// console.log(sumPairs([1,2,3,4,5], 7))
-// console.log(sumPairs(([1,2,3,4,5], 7)))
+console.log(sumPairs([1,2,3,4,5], 9))
+console.log(sumPairs([1,2,3,4,5], 7))
+console.log(sumPairs([3, 1, 5, 8, 2], 27))
